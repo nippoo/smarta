9504af63d41597082b9d9da8e73f6b2ec82300d4
Max Hunter <nippoo@nippoo.net>
2019-09-20 19:16:01 +0100

    Spruced up colours a bit

--- fetchdata.js
+++ fetchdata.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
       line_name = json[i]["name"]
       status = '<span class="status">' + json[i]["lineStatuses"][0]["statusSeverityDescription"] + '</span>'
       if (json[i]["lineStatuses"][0]["reason"]){
-        status += "<br>" + json[i]["lineStatuses"][0]["reason"]
+        status += '<br><span class="desc">' + json[i]["lineStatuses"][0]["reason"] + '</span>'
       }
       table.append('<tr><td class="tubeline ' + line_name + '">' + line_name + '</td><td>' + status + '</td></tr>' );
     }
@@ -35,19 +35,30 @@ $(document).ready(function(){
         bus_number = json[i]["lineName"]
         towards = json[i]["destinationName"]
         timetil = Math.floor(json[i]["timeToStation"] / 60)
-        table.append('<tr><td class="busnumber">' + bus_number + '</td class="towards"><td>' + towards + '</td><td class="timeuntil">' + timetil + '</td></tr>' );
+        table.append('<tr><td class="busnumber">' + bus_number + '</td><td class="towards">' + towards + '</td><td class="timeuntil">' + timetil + '</td></tr>' );
       }
 
       $('#bustimes').append(newdiv);
     });
   };
 
+  function getOccupancy(){
+    $.getJSON("https://wifi.huntershome.org:8443/api/login", {
+      username: unifi_u,
+      password: unifi_p,
+      strict: true
+        },  function(json) {
+        $('#dashboard').append(json)
+  })
+  }
+
+  getOccupancy();
   getBusTimes("490007530S");
   getBusTimes("490009155S");
   getBusTimes("490004174E");
   getBusTimes("490004991N");
 
     var d = new Date();
-    $('#dashboard').append("Last updated at " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds());
+    $('#footer').append("<p id='lastupdated'>Last updated at " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() + "</p>");
 
   });
